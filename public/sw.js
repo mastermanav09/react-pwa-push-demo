f0ba67344c2800706ec1deee2cb3db65414fe75c
manavnaharwal <manavnaharwalp@gmail.com>
2021-12-20 19:26:41 +0530

    test #1

--- public/sw.js
+++ public/sw.js
@@ -2,18 +2,16 @@ let cacheData = "appV1";
 
 this.addEventListener("install", (event) =>
   event.waitUntil(
-    caches
-      .open(cacheData)
-      .then((cache) =>
-        cache.addAll([
-          "static/js/bundle.js",
-          "static/js/main.chunk.js",
-          "static/js/0.chunk.js",
-          "/index.html",
-          "/",
-          "/users",
-        ])
-      )
+    caches.open(cacheData).then((cache) =>
+      cache.addAll([
+        "static/js/bundle.js",
+        "static/js/main.chunk.js",
+        // "static/js/0.chunk.js",
+        "/index.html",
+        "/",
+        "/users",
+      ])
+    )
   )
 );
 
