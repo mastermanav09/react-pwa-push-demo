5db529930a66299b2afa8aff5cb9e76102d62cdf
manavnaharwal <manavnaharwalp@gmail.com>
2021-12-20 19:30:02 +0530

    test #2

--- public/sw.js
+++ public/sw.js
@@ -4,12 +4,12 @@ this.addEventListener("install", (event) =>
   event.waitUntil(
     caches.open(cacheData).then((cache) =>
       cache.addAll([
-        "static/js/bundle.js",
-        "static/js/main.chunk.js",
-        // "static/js/0.chunk.js",
-        "/index.html",
-        "/",
-        "/users",
+        // "static/js/bundle.js",
+        // "static/js/main.chunk.js",
+        // // "static/js/0.chunk.js",
+        // "/index.html",
+        // "/",
+        // "/users",
       ])
     )
   )
