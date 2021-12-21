4ded9f364ec6c739cb35463de5817329cf348241
manavnaharwal <manavnaharwalp@gmail.com>
2021-12-21 20:25:26 +0530

    Fixed sw

--- src/push.js
+++ src/push.js
@@ -5,9 +5,8 @@ const webpush = require("web-push");
 // console.log(vapidKeys);
 
 let vapidKeys = {
-  publicKey:
-    "BGMi__fc21zU58eN9SL4_0b3K5-pAdTCwIB8vGtWWllk3izLbIFllwCHzK8CqCvpYf7Qw2PefqAgLBsyXhSCcDo",
-  privateKey: "syQa8AcHnwpp2h4UAaijlCfkyJZQ0XaJ68Ky4FhQywY",
+  publicKey: process.env.REACT_APP_PUSH_PUBLIC_VAPID_KEY,
+  privateKey: process.env.REACT_APP_PUSH_PRIVATE_VAPID_KEY,
 };
 
 webpush.setVapidDetails(
@@ -17,13 +16,11 @@ webpush.setVapidDetails(
 );
 
 let sub = {
-  endpoint:
-    "https://fcm.googleapis.com/fcm/send/c8l5kY6BNTc:APA91bH1uEn8vsPNITxETWyvPkChRUIoJUffKgSLP1defYVuseXDSOYqzc5jzdw99X8LUVZ-S_ZG4vKL2O3qeq966OsOkrGuyxpNhAhJpi6FQu7DRD9Li6j3M3spUYCUHJMiE0vSw9rd",
+  endpoint: process.env.REACT_APP_PUSH_ENDPOINT,
   expirationTime: null,
   keys: {
-    p256dh:
-      "BC2Qvfp6t1fJ2E-e_iCIvLWNt4iZSDyWLeH2DpAw7OxPkt3-IYd4dbKXUWs3OyVaJw8ffIjM9sVI8zgPItsVE-Y",
-    auth: "Qi64P4mZsJnNEJkCrljf8g",
+    p256dh: process.env.REACT_APP_KEYS_p256dh,
+    auth: process.env.REACT_APP_KEYS_auth,
   },
 };
 
