fe0b95e85721a913c182fb5b985fc0c88b9973fa
manavnaharwal <manavnaharwalp@gmail.com>
2021-12-22 14:03:48 +0530

    Fixed push

--- push.js
+++ push.js
@@ -5,8 +5,9 @@ const webpush = require("web-push");
 // console.log(vapidKeys);
 
 let vapidKeys = {
-  publicKey: process.env.REACT_APP_PUSH_PUBLIC_VAPID_KEY,
-  privateKey: process.env.REACT_APP_PUSH_PRIVATE_VAPID_KEY,
+  publicKey:
+    "BGMi__fc21zU58eN9SL4_0b3K5-pAdTCwIB8vGtWWllk3izLbIFllwCHzK8CqCvpYf7Qw2PefqAgLBsyXhSCcDo",
+  privateKey: "syQa8AcHnwpp2h4UAaijlCfkyJZQ0XaJ68Ky4FhQywY",
 };
 
 webpush.setVapidDetails(
@@ -16,7 +17,8 @@ webpush.setVapidDetails(
 );
 
 let sub = {
-  endpoint: process.env.REACT_APP_PUSH_ENDPOINT,
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/c8l5kY6BNTc:APA91bH1uEn8vsPNITxETWyvPkChRUIoJUffKgSLP1defYVuseXDSOYqzc5jzdw99X8LUVZ-S_ZG4vKL2O3qeq966OsOkrGuyxpNhAhJpi6FQu7DRD9Li6j3M3spUYCUHJMiE0vSw9rd",
   expirationTime: null,
   keys: {
     p256dh:
--- src/swDev.js
+++ src/swDev.js
@@ -8,9 +8,12 @@ export default async function swDev() {
 
 export async function subscribe() {
   let sw = await navigator.serviceWorker.ready;
-  let push = await sw.pushManager.subscribe({
-    userVisibleOnly: true,
-    applicationServerKey: process.env.REACT_APP_PUSH_PUBLIC_VAPID_KEY,
+  let push = await sw.pushManager.getSubscription().then((subscription) => {
+    return response.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: process.env.REACT_APP_PUSH_PUBLIC_VAPID_KEY,
+    });
   });
+
   console.log(push);
 }
